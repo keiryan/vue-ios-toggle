e508fe0c641d22147073938d210c43891bb5c73e
keiryan wilson <keiryanwilson@Max.local>
2023-12-21 22:56:21 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <ButtonToggle />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonToggle from "./components/buttonToggle.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ButtonToggle,
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+body { 
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #000;
+  color: #fff;
 }
 </style>
---
+++ src/components/buttonToggle.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :onClick="toggle" class="outside" :class="toggled && 'outside-toggled'">
+    <div class="inside" :class="toggled && 'inside-toggled'" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toggled: false,
+    };
+  },
+
+  methods: {
+    toggle() {
+      this.toggled = !this.toggled;
+      console.log(this.toggled)
+    },
+  },
+};
+</script>
+
+<style scoped>
+.outside {
+  width: 80px;
+  height: 50px;
+  background-color: #39383c;
+  border-radius: 25px;
+  position: relative;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  overflow: hidden;
+}
+
+.inside {
+  width: 40px;
+  height: 40px;
+  background-color: #fff;
+  border-radius: 50%;
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  transition: left 0.2s;
+  transition-timing-function: cubic-bezier(-1.36e-35, -3.26, 4.26, 1.00);
+}
+
+.inside-toggled {
+  left: calc(100% - 45px);
+}
+
+.outside-toggled {
+  background-color: #2FD158;
+}
+</style>
